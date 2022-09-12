09d3d0bb6ef9a8a50d6179266f451277b1794da6
Nikita Kravchenko <89710564+NicKravchenko@users.noreply.github.com>
2022-09-12 15:29:26 -0400

    Almost done insertion of subjects

--- SQL/PCI_data_insertion.sql
+++ SQL/PCI_data_insertion.sql
@@ -1,8 +1,10 @@
+USE ProjectIndiaCharlie;
+
 DECLARE @Path NVARCHAR(MAX);
 DECLARE @FileLoc NVARCHAR(MAX);
 DECLARE @SQL_BULK VARCHAR(MAX);
---SET @Path = 'C:\Users\Nikita\Desktop\Projects\ProjectIndiaCharlie\SQL\';--Path to folder of your pc for bulk insert script
-SET @Path = 'C:\Users\omars\source\repos\VxGameX\IDS325-01\ProjectIndiaCharlie\SQL\';--Path to folder of your pc for bulk insert script
+SET @Path = 'C:\Users\Nikita\Desktop\Projects\ProjectIndiaCharlie\SQL\';--Path to folder of your pc for bulk insert script
+--SET @Path = 'C:\Users\omars\source\repos\VxGameX\IDS325-01\ProjectIndiaCharlie\SQL\';--Path to folder of your pc for bulk insert script
 
 SET @FileLoc = @Path + 'Asignaturas.csv';
 
@@ -16,8 +18,66 @@ WITH (
 );' --  
 EXEC(@SQL_BULK);
 
-INSERT INTO Academic.Classroom(Code, IsLab, Capacity)
-VALUES ('FD402', 0, 20)
+
+SET @FileLoc = @Path + 'Persons.csv';
+
+SET @SQL_BULK = 'BULK INSERT  Person.Person
+FROM  ''' + @FileLoc + '''
+WITH (
+  FIELDTERMINATOR = '';'',
+  ROWTERMINATOR = ''\n'',
+  FIRSTROW = 2,
+  CODEPAGE = ''ACP''
+);' --  
+EXEC(@SQL_BULK);
+
+SET @FileLoc = @Path + 'Profesores.csv';
+
+SET @SQL_BULK = 'BULK INSERT  Academic.Professor
+FROM  ''' + @FileLoc + '''
+WITH (
+  FIELDTERMINATOR = '';'',
+  ROWTERMINATOR = ''\n'',
+  FIRSTROW = 2,
+  CODEPAGE = ''ACP''
+);' --  
+EXEC(@SQL_BULK);
+
+SET @FileLoc = @Path + 'SubjectDetail.csv';
+
+SET @SQL_BULK = 'BULK INSERT  Academic.SubjectDetail
+FROM  ''' + @FileLoc + '''
+WITH (
+  FIELDTERMINATOR = '';'',
+  ROWTERMINATOR = ''\n'',
+  FIRSTROW = 2,
+  CODEPAGE = ''ACP''
+);' --  
+EXEC(@SQL_BULK);
+
+SET @FileLoc = @Path + 'Classroom.csv';
+
+SET @SQL_BULK = 'BULK INSERT  Academic.Classroom
+FROM  ''' + @FileLoc + '''
+WITH (
+  FIELDTERMINATOR = '';'',
+  ROWTERMINATOR = ''\n'',
+  FIRSTROW = 2,
+  CODEPAGE = ''ACP''
+);' --  
+EXEC(@SQL_BULK);
+
+SET @FileLoc = @Path + 'SubjectClassroom.csv';
+
+SET @SQL_BULK = 'BULK INSERT  Academic.SubjectClassroom
+FROM  ''' + @FileLoc + '''
+WITH (
+  FIELDTERMINATOR = '';'',
+  ROWTERMINATOR = ''\n'',
+  FIRSTROW = 2,
+  CODEPAGE = ''ACP''
+);' --  
+EXEC(@SQL_BULK);
 
 INSERT INTO Academic.Grade(Grade, Points)
 VALUES ('A', 4),
@@ -35,75 +95,13 @@ VALUES	('Monday'),
 	('Thursday'),
 	('Friday'),
 	('Saturday');
-
-----Register cordinadores y  careras
---Medicina
-EXEC [Person].[SP_PersonRegistration]
-	@DocNo = N'38564444658',
-	@FirstName = N'María',
-	@MiddleName = N'Nicole',
-	@FirstSurname = N'Tejada',
-	@SecondSurname = N'Diaz',
-	@Gender = N'f',
-	@BirthDate = '1978-04-02',
-	@Email = N'Miguelrodrig@mail.net',
-	@RolId = 2,
-	@PasswordHash = N'e14ceeffc3107c5956645fe09232515ed7d2af3048eea37e2571bd340c7ef05a',
-	@PasswordSalt = N'jj331'
 GO
 
---Software
-EXEC [Person].[SP_PersonRegistration]
-	@DocNo = N'12345678901',
-	@FirstName = N'Roger',
-	@FirstSurname = N'Ramiro',
-	@Gender = N'm',
-	@BirthDate = '1988-02-10',
-	@Email = N'ramiro@mail.com',
-	@RolId = 2,
-	@PasswordHash = N'fdbfd43cca1aed1691d4bd7821d700449d53fd84f419462de98c6504da136687',
-	@PasswordSalt = N'8ui37'
+INSERT INTO Academic.Career(Name, Code, Subjects, Credits, Year, IsActive)
+VALUES ('Medicina', 'MED', 204, 425, 2020, 1),
+('Ingeniería de Software', 'IDS', 111, 279, 2020, 1),
+('Ingeniería Mecánica', 'MEC', 112, 280, 2020, 1);
 GO
-
---Ing mecanica
-EXEC [Person].[SP_PersonRegistration]
-	@DocNo = N'09876543210',
-	@FirstName = N'Oliver ',
-	@FirstSurname = N'Ledesma',
-	@Gender = N'm',
-	@BirthDate = '1979-11-9',
-	@Email = N'OliverLedesma@mail.com',
-	@RolId = 2,
-	@PasswordHash = N'fdbfd43cca1aed1691d4bd7821d700449d53fd84f419462de98c6504da136687',
-	@PasswordSalt = N'8ui37'
-GO
-
-----Registrar estudiantes
-EXEC [Person].[SP_PersonRegistration]
-	@DocNo = N'29387404672',
-	@FirstName = N'Raquel',
-	@FirstSurname = N'Rijo',
-	@Gender = N'f',
-	@BirthDate = '2000-01-01',
-	@Email = N'raquelita@mail.net',
-	@RolId = 1,
-	@CareerId = 1,
-	@PasswordHash = N'e14ceeffc3107c5956645fe09232515ed7d2af3048eea37e2571bd340c7ef05a',
-	@PasswordSalt = N'jj331'
-GO
---Agregar rol de profe
-EXEC [Person].[SP_PersonRegistration]
-	@DocNo = N'5468765884',
-	@FirstName = N'Raquel',
-	@FirstSurname = N'Rijo',
-	@Gender = N'f',
-	@BirthDate = '2000-01-01',
-	@Email = N'raquelita@mail.net',
-	@RolId = 2,
-	@PasswordHash = N'e14ceeffc3107c5956645fe09232515ed7d2af3048eea37e2571bd340c7ef05a',
-	@PasswordSalt = N'jj331'
-GO
-
 
 
 SELECT * FROM Person.Person;
@@ -113,14 +111,22 @@ SELECT * FROM Academic.Career;
 SELECT * FROM Academic.Classroom;
 SELECT * FROM Academic.Grade;
 SELECT * FROM Academic.Professor;
-SELECT * FROM Academic.Schedule;
-SELECT * FROM Academic.Section;
 SELECT * FROM Academic.Student;
+SELECT * FROM Academic.StudentSubject;
+
 SELECT * FROM Academic.Subject;
 SELECT * FROM Academic.SubjectClassroom;
-SELECT * FROM Academic.SubjectStudent;
+SELECT * FROM Academic.SubjectDetail;
+SELECT * FROM Academic.SubjectSchedule;
 SELECT * FROM Academic.Weekday;
 
+SELECT S.SubjectCode, S.Name, SD.Section, P.FirstName, P.FirstSurname
+FROM Academic.SubjectDetail SD
+JOIN ACADEMIC.Subject S ON SD.SubjectId = S.SubjectId
+JOIN Person.Person P ON SD.ProfessorID = P.PersonID
+WHERE S.Name LIKE '%vector%';
+
+--WHERE P.PersonID = 1110201
 	--UPDATE Person.Person 
 --SET 
 --    --DocNo = '40243310243',
