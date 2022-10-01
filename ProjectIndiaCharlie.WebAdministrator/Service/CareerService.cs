638e759e97825dfb1ac4a5b68fa1e922610e9172
Nikita Kravchenko <89710564+NicKravchenko@users.noreply.github.com>
2022-10-01 16:52:24 -0400

    Web student registration career select list

--- ProjectIndiaCharlie.WebAdministrator/Service/CareerService.cs
+++ ProjectIndiaCharlie.WebAdministrator/Service/CareerService.cs
@@ -11,12 +11,7 @@ namespace ProjectIndiaCharlie.WebAdministrator.Service
         private const string mediaType = "application/json";
 
         private static string createCareerURL = $"{baseUrl}/Admin/CareerRegistration";
-        private static string getSchedule = $"{baseUrl}/Student/Schedule?studentId={{0}}";
-
-        private static string registerStudentUrl = $"{baseUrl}/Student/Registration";
-        private static string getUnresolvedRevisions = $"{baseUrl}/Admin/GetUnsolvedRevisions";
-        private static string getGrades = $"{baseUrl}/Academic/GradesList";
-        private static string processRevisionURL = $"{baseUrl}/Admin/ProcessGradeRevisions?studentID={{0}}&subjectDetailID={{1}}&modifiedgradeId={{2}}&adminId={{3}}";
+        private static string getCareersURL = $"{baseUrl}/Admin/CareersList";
 
         private static readonly JsonSerializerOptions _options = new() { PropertyNameCaseInsensitive = true };
 
@@ -42,6 +37,25 @@ namespace ProjectIndiaCharlie.WebAdministrator.Service
             catch (Exception)
             {
                 return null;
+            }
+        }
+        public static async Task<IEnumerable<VAvailableCareer>> GetCareers()
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+                var response = await httpClient.GetAsync(string.Format(getCareersURL));
+
+                if (!response.IsSuccessStatusCode)
+                    return Enumerable.Empty<VAvailableCareer>();
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                return JsonSerializer.Deserialize<IEnumerable<VAvailableCareer>>(content, _options)!;
+            }
+            catch (Exception)
+            {
+                return Enumerable.Empty<VAvailableCareer>();
             }
         }
     }
